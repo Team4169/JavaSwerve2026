3a407abf153976d3f53d907f01a9f74adc6daae2
Matt Yershov <mattyershov@gmail.com>
2026-01-27 17:26:56 -0500

    Edited the code to use SparkMaxes and NEOs, which we will use for the shooter prototype.

--- motorcontrol/src/main/java/frc/robot/Robot.java
+++ motorcontrol/src/main/java/frc/robot/Robot.java
@@ -7,10 +7,10 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-// import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax; not needed because the motor control robot uses Talon SRXs
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
 
 /**
  * This sample program shows how to control a motor using a joystick. In the operator control part
@@ -23,31 +23,19 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
  * to the Dashboard.
  */
 public class Robot extends TimedRobot {
-  // below are the CAN IDs for the motors and joysticks. We did not use encoders.
-  private static final int kMotorPort1 = 3;
-  private static final int kMotorPort2 = 4;
-  private static final int kMotorPort3 = 8;
-  private static final int kMotorPort4 = 9;
-  private static final int kMotorPort5 = 51;
+  // below are the CAN IDs for the motors and the joystick port. We did not use encoders.
+  private static final int kMotorPort = 3;
   private static final int kJoystickPort = 0;
   private static final int kEncoderPortA = 0;
   private static final int kEncoderPortB = 1;
 
-  private final TalonSRX m_motor1;
-  private final TalonSRX m_motor2;
-  private final TalonSRX m_motor3;
-  private final TalonSRX m_motor4;
-  private final TalonSRX m_motor5;
+  private final SparkMax m_motor1;
   private final Joystick m_joystick;
   private final Encoder m_encoder;
 
   /** Called once at the beginning of the robot program. */
   public Robot() {
-    m_motor1 = new TalonSRX(kMotorPort1);
-    m_motor2 = new TalonSRX(kMotorPort2);
-    m_motor3 = new TalonSRX(kMotorPort3);
-    m_motor4 = new TalonSRX(kMotorPort4);
-    m_motor5 = new TalonSRX(kMotorPort5);
+    m_motor1 = new SparkMax(kMotorPort, MotorType.kBrushless);
     m_joystick = new Joystick(kJoystickPort);
     m_encoder = new Encoder(kEncoderPortA, kEncoderPortB);
     // Use SetDistancePerPulse to set the multiplier for GetDistance
@@ -67,11 +55,6 @@ public class Robot extends TimedRobot {
   /** The teleop periodic function is called every control packet in teleop. */
   @Override
   public void teleopPeriodic() {
-    m_motor1.set(ControlMode.PercentOutput, m_joystick.getY());
-    m_motor2.set(ControlMode.PercentOutput, m_joystick.getY());
-    m_motor3.set(ControlMode.PercentOutput, m_joystick.getY());
-    m_motor4.set(ControlMode.PercentOutput, m_joystick.getY());
-    // System.out.println(m_joystick.getY());
-    m_motor5.set(ControlMode.PercentOutput, m_joystick.getY());
+    m_motor1.set(m_joystick.getY());
   }
 }
