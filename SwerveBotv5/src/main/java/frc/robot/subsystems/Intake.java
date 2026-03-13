e5521a59b457cc3ba1ca47d5e2d64373bdcc6f4f
MassHiker <148277895+MassHiker@users.noreply.github.com>
2026-03-13 09:50:44 -0400

    New V5 version that accounts for through bore encoders

---
+++ SwerveBotv5/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class Intake extends SubsystemBase {
+
+  private static final int kIntakeMotorPort = 7;
+  private static final int kFoldMotorPort = 2;
+
+  private final SparkMax m_intakeMotor;
+  private final SparkMax m_foldMotor;
+
+  public Intake() {
+    m_intakeMotor = new SparkMax(kIntakeMotorPort, MotorType.kBrushless);
+    m_foldMotor = new SparkMax(kFoldMotorPort, MotorType.kBrushless);
+  }
+
+  public Command runIntake() {
+    return runOnce(this::startIntakeMotor);
+  }
+
+  public Command runIntakeHeld() {
+    return startEnd(this::startIntakeMotor, this::stopIntakeMotor);
+  }
+
+  public Command foldupIntake() {
+    return runOnce(this::startFoldUpMotor);
+  }
+
+  public Command foldUpHeld() {
+    return startEnd(this::startFoldUpMotor, this::stopFoldMotor);
+  }
+
+  public Command folddownIntake() {
+    return runOnce(this::startFoldDownMotor);
+  }
+
+  public Command foldDownHeld() {
+    return startEnd(this::startFoldDownMotor, this::stopFoldMotor);
+  }
+
+  private void startIntakeMotor() {
+    m_intakeMotor.set(-0.8);
+  }
+
+  private void stopIntakeMotor() {
+    m_intakeMotor.stopMotor();
+  }
+
+  private void startFoldUpMotor() {
+    m_foldMotor.set(-0.1);
+  }
+
+  private void startFoldDownMotor() {
+    m_foldMotor.set(0.1);
+  }
+
+  private void stopFoldMotor() {
+    m_foldMotor.stopMotor();
+  }
+}
